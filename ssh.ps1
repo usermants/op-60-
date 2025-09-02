b77981dea6f737df9762a60cf765bfd0f32febff
usermants <sanchezguy@proton.me>
2025-09-02 14:04:14 -0300

    Atualizar o ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,1 +1,1 @@
-ssh -i "C:\Windows\System32\OpenSSH\revssh_key" -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL logosofico@0.tcp.sa.ngrok.io -p 10660 -N -R 2222:localhost:22
+ssh -i "C:\Windows\System32\OpenSSH\revssh_key" -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL logosofico@0.tcp.sa.ngrok.io -p 11041 -N -R 2222:localhost:22
