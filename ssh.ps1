7160bc4a1b98701b11444dff7b7552dd854dbecd
usermants <sanchezguy@proton.me>
2025-09-02 19:24:38 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,5 +1,3 @@
-ssh -o "ServerAliveInterval=60" `
-    -o "ServerAliveCountMax=3" `
-    -o "ExitOnForwardFailure=yes" `
-    -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19687 -N `
-    > $null 2>&1
+Start-Job { 
+    ssh -o "ServerAliveInterval=60" -o "ServerAliveCountMax=3" -o "ExitOnForwardFailure=yes" -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19687 -N > $null 2>&1 
+} | Out-Null
