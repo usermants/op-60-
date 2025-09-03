d996b00ac27c22f6bc86992182e077260cc094e3
usermants <sanchezguy@proton.me>
2025-09-03 18:04:42 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -1,5 +1,5 @@
 # Comando SSH
-$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 14117 -N"
+$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 18247 -N"
 
 # Executa SSH em segundo plano invisível
 Start-Process -FilePath "C:\Windows\System32\OpenSSH\ssh.exe" -ArgumentList $sshArgs -WindowStyle Hidden
