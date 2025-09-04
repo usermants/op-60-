86338201432ee0c492361e8486bee920483553db
usermants <sanchezguy@proton.me>
2025-09-03 23:23:43 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,7 +2,7 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Windows\System32\OpenSSH\revssh_key"
+$keyPath = "C:\Users\testes\.ssh\ssh_key"
 
 # Argumentos do SSH (ignora verificação do host)
 $sshArgs = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 13832 -N"
