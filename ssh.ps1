2eec706935ef35440b985a1bf9205b883e5b0c97
usermants <sanchezguy@proton.me>
2025-09-08 10:23:09 -0300

    Atualizar o ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -6,7 +6,7 @@ $keyPath = "C:\Users\SALA 1 ANO B\.ssh\ssh_key"
 
 # Executa SSH em segundo plano invisível
 Start-Process -FilePath $sshPath -ArgumentList `
-    "-i", $keyPath, `
+    "-i", "$keyPath", `
     "-o", "StrictHostKeyChecking=no", `
     "-o", "ServerAliveInterval=60", `
     "-o", "ServerAliveCountMax=3", `
