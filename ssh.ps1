9f962d9ed87a2ceed285452a9836f07e9f3da1e2
usermants <sanchezguy@proton.me>
2025-09-09 01:31:17 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,27 +2,21 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Users\Testes\.ssh\ssh_key" 
+$keyPath = "C:\Users\Testes\.ssh\ssh_key"
 
-# Caminhos dos arquivos de log
-$stdoutLog = "C:\Users\Testes\ssh_stdout_log.txt"
-$stderrLog = "C:\Users\Testes\ssh_stderr_log.txt"
+# Caminho do log (único arquivo com stdout + stderr)
+$logFile = "C:\Users\Testes\.ssh\ssh_verbose_log.txt"
 
-# Argumentos do SSH
-$arguments = @(
-    "-i", "$keyPath",
-    "-o", "StrictHostKeyChecking=no",
-    "-o", "ServerAliveInterval=60",
-    "-o", "ServerAliveCountMax=3",
-    "-o", "ExitOnForwardFailure=yes",
-    "-R", "2222:localhost:22",
-    "u0_a416@0.tcp.sa.ngrok.io", 
-    "-p", "9999999",
-    "-N",
-    "-vvv"
-)
+# Monta os argumentos exatamente como no terminal
+$arguments = @"
+-i `"$keyPath`" `
+-o StrictHostKeyChecking=no `
+-o ServerAliveInterval=60 `
+-o ServerAliveCountMax=3 `
+-o ExitOnForwardFailure=yes `
+-R 2222:localhost:22 `
+u0_a416@0.tcp.sa.ngrok.io -p 18149 -N -vvv *>> `"$logFile`"
+"@
 
-# Executa SSH invisível e redireciona saída e erros para arquivos separados
-Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden `
-    -RedirectStandardOutput $stdoutLog `
-    -RedirectStandardError $stderrLog
+# Executa SSH em segundo plano invisível com redirecionamento para log
+Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
