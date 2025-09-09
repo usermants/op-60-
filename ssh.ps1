17a8537c163d3051f24256bb3a249ef5173a9973
usermants <sanchezguy@proton.me>
2025-09-09 08:47:53 -0300

    Atualizar o ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -2,10 +2,10 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Users\Testes\.ssh\ssh_key"
+$keyPath = "C:\Users\SALA 1 ANO B\.ssh\ssh_key"
 
 # Caminho do log
-$logFile = "C:\Users\Testes\.ssh\ssh_verbose_log.txt"
+$logFile = "C:\Users\SALA 1 ANO B\.ssh\ssh_verbose_log.txt"
 
 # Monta o comando exatamente como no terminal
 $command = "& `"$sshPath`" -i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a416@0.tcp.sa.ngrok.io -p 18149 -N -vvv *>> `"$logFile`""
