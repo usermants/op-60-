552a08887042ee6dfa1fbb2515a0da120061137f
usermants <sanchezguy@proton.me>
2025-09-16 14:44:31 -0300

    Atualizar o ssh.ps1

--- ssh.ps1
+++ ssh.ps1
@@ -5,7 +5,7 @@ $sshPath = "C:\Program Files\OpenSSH\ssh.exe"
 $keyPath = "C:\Users\SALA 1 ANO B\.ssh\ssh_key"
 
 # Argumentos da conexão SSH
-$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 15585 -N -vvv"
+$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 2222:localhost:22 u0_a434@0.tcp.sa.ngrok.io -p 15354 -N -vvv"
 
 # Executa o SSH de forma invisível
 Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
