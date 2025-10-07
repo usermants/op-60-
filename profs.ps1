bb4f5f1ad10d6e208b1ee3bbab2254b938a5b332
usermants <sanchezguy@proton.me>
2025-10-07 09:58:27 -0300

    Atualizar o profs.ps1

--- profs.ps1
+++ profs.ps1
@@ -2,7 +2,7 @@
 $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 
 # Caminho da chave privada
-$keyPath = "C:\Users\Professores 2\.ssh\ssh_key"
+$keyPath = "C:\Users\Admin\.ssh\ssh_key"
 
 # Argumentos da conexão SSH
 $arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 3333:localhost:22 u0_a434@0.tcp.sa.ngrok.io -p 11589 -N -vvv"
