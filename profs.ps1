ced3613c6bbcd9e91b4a57cae49cec656f33c31f
usermants <sanchezguy@proton.me>
2025-10-07 09:35:15 -0300

    Atualizar o profs.ps1

--- profs.ps1
+++ profs.ps1
@@ -5,7 +5,7 @@ $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 $keyPath = "C:\Users\Professores 2\.ssh\ssh_key"
 
 # Argumentos da conexão SSH
-$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 3333:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 13793 -N -vvv"
+$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 3333:localhost:22 u0_a434@0.tcp.sa.ngrok.io -p 11589 -N -vvv"
 
 # Executa o SSH de forma invisível
 Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
